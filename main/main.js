a400c72dd4c89fc7b1f93ca18e5f6ca9d344d8c1
Chen Xu <xuchen@thoughtworks.com>
2020-10-30 16:33:48 +0800

    [chenxu]: refactor

--- main/main.js
+++ main/main.js
@@ -1,43 +1,6 @@
 module.exports = function printInventory(inputs) {
-    let result = '';
-
-    function generateOneItemInfo(item) {
-        let result = `Name: ${item.Name}, Quantity: ${item.Quantity}`
-        if (item.Unit) {
-            result += ` ${item.Unit}`;
-        }
-        result += `, Unit price: ${Number(item.UnitPrice).toFixed(2)} (yuan), Subtotal: ${Number(item.Total).toFixed(2)} (yuan)\n`;
-
-        return result;
-    }
-
-    function generateTotalInfo(items) {
-        let total = items.reduce((sumTotal, current) => {
-            sumTotal += current.Total;
-            return sumTotal;
-        }, 0);
-        return `Total: ${Number(total).toFixed(2)} (yuan)\n`;
-    }
-
-    let reducedInput = inputs.reduce((groupInputs, current) => {
-        let findItem = groupInputs.find(item => item.Name === current.Name);
-
-        if (findItem == null) {
-            findItem = {};
-            findItem.Total = 0;
-            findItem.UnitPrice = current.Price;
-            findItem.Unit = current.Unit === 'bottle' ? 'bottles' : '';
-            findItem.Quantity = 0;
-            groupInputs.push(findItem);
-        }
-
-        findItem.Name = current.Name;
-        findItem.Total += current.Price;
-        findItem.Quantity++;
-
-        return groupInputs;
-    }, []);
-
+    
+    let reducedInput = groupAndProcessInput(inputs);
     let detail = ''
     reducedInput.forEach(element => {
         return detail += generateOneItemInfo(element);
@@ -50,5 +13,46 @@ module.exports = function printInventory(inputs) {
         '----------------------\n' +
         total +
         '**********************\n';
+
+        function generateOneItemInfo(item) {
+            let result = `Name: ${item.Name}, Quantity: ${item.Quantity}`
+            if (item.Unit) {
+                result += ` ${item.Unit}`;
+            }
+            result += `, Unit price: ${Number(item.UnitPrice).toFixed(2)} (yuan), Subtotal: ${Number(item.Total).toFixed(2)} (yuan)\n`;
+    
+            return result;
+        }
+    
+        function generateTotalInfo(items) {
+            let total = items.reduce((sumTotal, current) => {
+                sumTotal += current.Total;
+                return sumTotal;
+            }, 0);
+            return `Total: ${Number(total).toFixed(2)} (yuan)\n`;
+        }
+    
+        function groupAndProcessInput(input){
+            let processedInput = inputs.reduce((groupInputs, current) => {
+                let findItem = groupInputs.find(item => item.Name === current.Name);
+        
+                if (findItem == null) {
+                    findItem = {};
+                    findItem.Total = 0;
+                    findItem.UnitPrice = current.Price;
+                    findItem.Unit = current.Unit === 'bottle' ? 'bottles' : '';
+                    findItem.Quantity = 0;
+                    groupInputs.push(findItem);
+                }
+        
+                findItem.Name = current.Name;
+                findItem.Total += current.Price;
+                findItem.Quantity++;
+        
+                return groupInputs;
+            }, []);
+    
+            return processedInput;
+        }
 };
 
